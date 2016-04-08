c50aeb77d0c681271865989a0936da8729dc507d
svangaal <svangaal@gmail.com>
2016-04-08 10:41:23 -0400

    Proposed solution
    Proposed solution, similar to those found on message board.
    
    Both my solution as well as the example solution generate a message that I cannot find on the message board: Error in x$getmean : $ operator is invalid for atomic vectors
    
    Not sure what to make of this

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is my solution to the Programming in R week 2 assignment
+## It is intended to check for a previously calculated solution
+## to a matrix inversion, and if none is available, to calculate it
 
-## Write a short comment describing this function
+## This function prepares a list of four functions that can be used 
+## to: 
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse
+## 4. Get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) inv <<- solve
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function takes as input the matrix that is to be inverted
+## First it checks to see if the inversion has already been performed and 
+## the result chached. If so, it uses that value. 
+## If not, it calculates the value, caches the value, and returns the value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
